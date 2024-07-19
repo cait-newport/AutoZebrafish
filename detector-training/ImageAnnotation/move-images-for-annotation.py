96b8cf7761121f2c7e6606a5c4bc45a9aa2d0af5
Olly King <olly.king@gmail.com>
2024-07-19 15:19:29 +0100

    Changes move-images script to read in command line args

--- detector-training/ImageAnnotation/move-images-for-annotation.py
+++ detector-training/ImageAnnotation/move-images-for-annotation.py
@@ -1,19 +1,26 @@
-# Read in the following file: /Users/user/projects/AutoZebrafish/Detector training/ImageAnnotation/sample_files_list.txt
-# For each file in this list, copy the file and move it to a new directory: /Volumes/RFS/Triggerfish Navigation/Zebrafish/annotation/
-# Keep the folder structure after allframes/ and before the file name
-
-import os
 import shutil
+import click
+from pathlib import Path
+import logging
 
-source_file = "/Users/user/projects/AutoZebrafish/Detector training/ImageAnnotation/sample_files_list.txt"
-destination_dir = "/Volumes/RFS/Triggerfish Navigation/Zebrafish/annotation/"
+logging.basicConfig(level=logging.INFO)
 
-with open(source_file, "r") as file:
-    for line in file:
-        file_path = line.strip()
-        if os.path.isfile(file_path):
-            folder_path = os.path.dirname(file_path)
-            folder_structure = folder_path.split("allframes/")[1]
-            destination_folder = os.path.join(destination_dir, folder_structure)
-            os.makedirs(destination_folder, exist_ok=True)
-            shutil.copy(file_path, destination_folder)
+@click.command()
+@click.argument('source_file', type=click.Path(exists=True))
+@click.argument('destination_dir', type=click.Path())
+def move_images(source_file, destination_dir):
+    source_file = Path(source_file)
+    destination_dir = Path(destination_dir)
+    logging.info(f"Moving images listed in {source_file} to {destination_dir}")
+    with open(source_file, "r") as file:
+        for line in file:
+            file_path = line.strip()
+            if Path(file_path).is_file():
+                folder_path = Path(file_path).parent
+                folder_structure = folder_path.relative_to(folder_path.parent)
+                destination_folder = destination_dir / folder_structure
+                destination_folder.mkdir(parents=True, exist_ok=True)
+                shutil.copy(file_path, destination_folder)
+
+if __name__ == '__main__':
+    move_images()
